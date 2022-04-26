5dd1dbcee15f29f160a1abf1bd2b9835146e9da4
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-04-26 08:19:54 +0000

    fix roles attr on create new user and disable grant for users routes

--- routes/web.php
+++ routes/web.php
@@ -45,5 +45,5 @@ Route::middleware(['auth'])->group(function () {
 
 //Route::middleware(['auth'])->group(function () {
     Route::resource('roles', RoleController::class)->middleware(['role:superadmin']);
-    Route::resource('users', UserController::class)->middleware(['role:admin']);
+    Route::resource('users', UserController::class)->middleware([]);
 //});
